6b61fb654ce30abc85cddfec4ff7c6114fe10cdd
johnmyamamoto <142952678+johnmyamamoto@users.noreply.github.com>
2024-09-22 22:54:06 -1000

    Added sumofnumbers files

---
+++ sumofnumbers.js
@@ -0,0 +1,32 @@
+const testArray = [1, 4, 7, 4, 8];
+function sumFor(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+console.log(sumFor(testArray));
+function sumWhile(arr) {
+    let sum = 0;
+    let i = 0;
+    while (i < arr.length) {
+        sum += arr[i];
+        i++;
+    }
+    return sum;
+}
+console.log(sumWhile(testArray));
+function sumRecursion(arr) {
+    if (arr.length === 0) {
+        return 0;
+    }
+    return arr[0] + sumRecursion(arr.slice(1));
+}
+console.log(sumRecursion(testArray));
+function sumTheFunctionalWay(arr) {
+    let sum = 0;
+    arr.forEach((item) => sum += item);
+    return sum;
+}
+console.log(sumTheFunctionalWay(testArray));
